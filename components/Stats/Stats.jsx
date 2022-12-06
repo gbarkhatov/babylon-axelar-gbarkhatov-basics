076223923d8a92bd0362cba99dad11caec122a21
Govard Barkhatov <gbarkhatov@gmail.com>
2022-12-06 19:25:02 +0700

    update font sizes

--- components/Stats/Stats.jsx
+++ components/Stats/Stats.jsx
@@ -3,8 +3,10 @@ const Stats = ({ title, value, description }) => (
     <div className='stats shadow flex flex-1'>
       <div className='stat'>
         {title && <div className='stat-title'>{title}</div>}
-        {value && <div className='stat-value'>{value}</div>}
-        {description && <div className='stat-desc'>{description}</div>}
+        {value && <div className='stat-value text-2xl text-left'>{value}</div>}
+        {description && (
+          <div className='stat-desc text-left'>{description}</div>
+        )}
       </div>
     </div>
   </div>
